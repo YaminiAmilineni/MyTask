9be96c3a151bde40757af62c0b49015211dcf066
YaminiAmilineni <yaminiak1996@gmail.com>
2021-11-30 13:07:53 +0530

    EnemySpawn

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,20 +11,28 @@ public class EnemySpawner : MonoBehaviour
     public float timer = 2f;
 
 
-
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SpawnEnemies", timer);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnEnemies()
     {
-        
+        float pos_Y = Random.Range(min_Y, max_Y);
+        Vector3 temp = transform.position;
+            temp.y = pos_Y;
+
+        if(Random.Range(0 ,2)> 0)
+        {
+            Instantiate(asteroid_Prefabs[Random.Range(0, asteroid_Prefabs.Length)], temp, Quaternion.identity);
+
+        }
+        else
+        {
+            Instantiate(enemyPrefab, temp, Quaternion.Euler(0f, 0f, -90f));
+        }
+
+        Invoke("SpawnEnemies", timer);
     }
 }
