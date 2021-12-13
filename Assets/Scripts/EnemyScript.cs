6bbc16fd2ce2a17b93d08a1d4492fdf9b4e31fa7
YaminiAmilineni <yaminiak1996@gmail.com>
2021-12-13 12:52:01 +0530

    commit1

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    pubic float speed = 5f;
+    pubic float rotate_Speed = 50f;
+
+    public bool canShoot;
+    public bool canRotate;
+    private bool canMove = true;
+
+    public float bound_X = -11f;
+    public Transform attack_Point;
+    public GameObject enemy_bullet_;
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if(canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.x < bound_X)
+                gameObject.SetActive(false);
+
+        }
+    }
+
+
+
+
+}
